82ddf8427eb949776a5824a91fa4b7b92ee180bf
Test67-gif <ori.hanani@gmail.com>
2025-12-04 21:54:10 +0200

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Mental-Math/',
+  base: '/business-math-trainer/',
 })
 
